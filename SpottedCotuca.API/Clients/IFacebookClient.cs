057e7ca20cdf989602057a15c6a390fa110afe1a
Lorenzo Pincinato <lorenzopincinato@gmail.com>
2019-02-24 23:47:32 -0300

    Add social clients interface

---
+++ SpottedCotuca.API/Clients/IFacebookClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SpottedCotuca.API.Clients
+{
+    public interface IFacebookClient
+    {
+        Task<long> CreatePost(string message);
+        Task<bool> DeletePost(long id);
+    }
+}
---
+++ SpottedCotuca.API/Clients/ITwitterClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace SpottedCotuca.API.Clients
+{
+    public interface ITwitterClient
+    {
+        Task<long> PublishTweet(string status);
+        Task<bool> DestroyTweet(long id);
+    }
+}
