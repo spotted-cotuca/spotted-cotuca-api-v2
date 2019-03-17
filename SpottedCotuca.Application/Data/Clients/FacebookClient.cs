97a535731db2f158ede0b7a7b9b496154ddcd8ef
Lorenzo Pincinato <lorenzopincinato@gmail.com>
2019-03-17 00:18:26 -0300

    Add FacebookClient implementation

--- SpottedCotuca.Application/Data/Clients/FacebookClient.cs
+++ SpottedCotuca.Application/Data/Clients/FacebookClient.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace SpottedCotuca.Application.Data.Clients
 {
     public class FacebookClient : IFacebookClient
     {
+        private readonly string _facebookApiUrl = "https://graph.facebook.com/";
         private string _pageId;
         private string _accessToken;
 
@@ -16,12 +20,80 @@ namespace SpottedCotuca.Application.Data.Clients
 
         public async Task<long> CreatePost(string message)
         {
-            throw new NotImplementedException();
+            var data = new Dictionary<string, string> {
+                { "message", message }
+            };
+
+            var response = await SendPostRequest("feed", data);
+
+            if (response.IsSuccessStatusCode)
+                return JsonConvert.DeserializeObject<FacebookResponse>(response.Content.ToString()).Id.ToFacebookPostId();
+
+            throw new Exception(response.Content.ToString());
         }
 
         public async Task<bool> DeletePost(long id)
         {
-            throw new NotImplementedException();
+            var response = await SendDeleteRequest($"{id}");
+
+            if (response.IsSuccessStatusCode)
+                return JsonConvert.DeserializeObject<FacebookResponse>(response.Content.ToString()).Success;
+
+            throw new Exception(response.Content.ToString());
         }
+
+        private async Task<HttpResponseMessage> SendPostRequest(string url, Dictionary<string, string> data)
+        {
+            var fullUrl = $"{_facebookApiUrl}{_pageId}/{url}";
+
+            data.Add("access_token", _accessToken);
+
+            var formData = new FormUrlEncodedContent(data);
+
+            using (var http = new HttpClient())
+            {
+                var response = await http.PostAsync(fullUrl, formData);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response;
+                }
+
+                throw new Exception(response.Content.ToString());
+            }
+        }
+
+        private async Task<HttpResponseMessage> SendDeleteRequest(string url)
+        {
+            var fullUrl = $"{_facebookApiUrl}{_pageId}_{url}?access_token={_accessToken}";
+
+            using (var http = new HttpClient())
+            {
+                var response = await http.DeleteAsync(fullUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return response;
+                }
+
+                throw new Exception(response.Content.ToString());
+            }
+        }
+    }
+
+    internal static class FacebookClientExtensions
+    {
+        public static long ToFacebookPostId(this string pagePostId)
+        {
+            return Convert.ToInt64(pagePostId.Substring(pagePostId.IndexOf('_') + 1));
+        }
+    }
+
+    internal class FacebookResponse
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        [JsonProperty("sucess")]
+        public bool Success { get; set; }
     }
 }
